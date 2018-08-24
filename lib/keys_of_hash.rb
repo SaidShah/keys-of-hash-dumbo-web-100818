2384f68d50cf3e3452592fc0d443e920aab9a459
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-24 01:17:21 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,12 @@ require "pry"
 
 class Hash
   def keys_of(*arguments)
- 
+    new_array = []
+     arguments.each do |animal,value|
+      if arguments.include?(value)
+        new_array << animal   
+    end
+   end
   end
 
 end
